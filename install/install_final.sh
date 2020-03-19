c5b3794ea4bf58f814ee995b43c2518f6bf447ad
EttOrm <40430949+EttOrm@users.noreply.github.com>
2020-03-19 16:41:42 +0100

    Update install_final.sh

--- install/install_final.sh
+++ install/install_final.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-#export LANGUAGE=en_US.UTF-8
-#export LC_ALL=en_US.UTF-8
-#export LANG=en_US.UTF-8
-#export LC_CTYPE=en_US.UTF-8
-
 cd /
-
-#sudo apt-get install autoconf -y
-#sudo apt-get install autotools-dev -y
 
 sudo apt-get install autoconf automake libtool
 
@@ -38,14 +30,11 @@ echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sourc
 sudo apt update -y
 sudo apt install -y php7.4-common php7.4-fpm php7.4-cli php7.4-json php7.4-mysql php7.4-opcache php7.4-sqlite3 php7.4-readline php-pear
 
-cd /home/pi/libcoap
-
-npm install node-tradfri --save
-npm audit fix
-
-cd /home/pi/
-
-npm install node-tradfri --save
-npm audit fix
+git clone --depth 1 --recursive -b dtls https://github.com/home-assistant/libcoap.git
+cd libcoap
+./autogen.sh
+./configure --disable-documentation --disable-shared --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
+make
+make install
 
 cd ~/NetDetect/
