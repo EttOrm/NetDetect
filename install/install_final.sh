1a811d85c1fce46e9872432f4b654a3a53b20599
EttOrm <40430949+EttOrm@users.noreply.github.com>
2020-03-19 16:44:53 +0100

    Update install_final.sh

--- install/install_final.sh
+++ install/install_final.sh
@@ -17,6 +17,13 @@ cd ../../
 make
 sudo make install
 
+git clone --depth 1 --recursive -b dtls https://github.com/home-assistant/libcoap.git
+cd libcoap
+./autogen.sh
+./configure --disable-documentation --disable-shared --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
+make
+make install
+
 sudo git clone https://github.com/royhills/arp-scan.git
 cd arp-scan 
 autoreconf --install 
@@ -29,12 +36,5 @@ wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
 echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php7.list
 sudo apt update -y
 sudo apt install -y php7.4-common php7.4-fpm php7.4-cli php7.4-json php7.4-mysql php7.4-opcache php7.4-sqlite3 php7.4-readline php-pear
-
-git clone --depth 1 --recursive -b dtls https://github.com/home-assistant/libcoap.git
-cd libcoap
-./autogen.sh
-./configure --disable-documentation --disable-shared --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
-make
-make install
 
 cd ~/NetDetect/
